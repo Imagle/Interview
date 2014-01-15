99c132abb62077b59f4406f9aeb2486dd522902c
Imagle <wangrq2008@gmail.com>
2014-01-15 20:30:06 +0800

    string class

--- interview/string/main.cc
+++ interview/string/main.cc
@@ -2,11 +2,23 @@
 #include <stdlib.h>
 #include "qh_string.h"
 
-
 int main(int argc, char* argv[])
 {
     //TODO 在这里添加单元测试，越多越好，代码路径覆盖率越全越好
 
+    char *p = "hello world!";
+    qh::string s1(p);
+    printf("s1 = [%s]\n", s1.c_str());
+    qh::string s2("how are you ?");
+    printf("s2 = [%s]\n", s2.data());
+    char * q = s2[2];
+    printf("q is %c\n", *q);
+    *q = 'G';
+    printf("s2 = [%s]\n", s2.data());
+    qh::string s3;
+    printf("%s\n", s3.data());
+    qh::string s4(s2);
+    printf("s4 = [%s]\n", s4.data());
 #ifdef WIN32
     system("pause");
 #endif
--- interview/string/qh_string.cc
+++ interview/string/qh_string.cc
@@ -7,31 +7,47 @@ namespace qh
     // TODO 将在这里写实现代码
 
     string::string()
-        : data_(NULL), len_(0)
+        : data_(new char[1]), len_(0)
     {
+        data_[0] = '\0';
     }
 
     string::string( const char* s )
+        : data_(new char[strlen(s)+1]), len_(strlen(s))
     {
+        strcpy(data_, s);
     }
 
     string::string( const char* s, size_t len )
+    :data_(new char[len+1]), len_(len)
     {
+        strncpy(data_, s, len);
     }
 
     string::string( const string& rhs )
+    :data_(NULL), len_(0)
     {
-
+        *this = rhs;
+        len_ = rhs.size();
     }
 
     string& string::operator=( const string& rhs )
     {
+        if(this == &rhs)
+            return  *this;
+        delete[] data_;
+        data_ = new char[rhs.size()+1];
+        strcpy(data_, rhs.data_);
         return *this;
     }
 
     string::~string()
     {
-
+        if(data_ != NULL){
+            delete[] data_;
+            data_ = NULL;
+            len_ = 0;
+        }
     }
 
     size_t string::size() const
@@ -41,16 +57,22 @@ namespace qh
 
     const char* string::data() const
     {
-        return NULL;
+        char* temp = new char[len_];
+        int i=0;
+        for(; i<len_;i++)
+            temp[i] = data_[i];
+        return temp;
     }
 
     const char* string::c_str() const
     {
-        return NULL;
+        return data_;
     }
 
     char* string::operator[]( size_t index )
     {
-        return NULL;
+        if(index > len_-1 || index<0)
+            return NULL;
+        return &data_[index];
     }
 }
